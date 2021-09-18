cbd8d24dbad7dafdd9cdafaaee46e88a7608d43b
sabudilovskiy <sabudilovskiy@yandex.ru>
2021-09-18 11:54:40 +0300

    ну на

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include "src/List.h"
+#include "src/List.cpp"
+#include "src/Node.cpp"
 int main() {
-    List temp_list;
+    List<int> temp_list;
+    auto lambda = [](int a){return std::to_string(a);};
     for (int i = 0; i < 10; i++) temp_list.push_back(i);
-    std::cout << temp_list.to_string()<< '\n';
-    List subList;
+    std::cout << temp_list.to_string(lambda)<< '\n';
+    List<int> subList;
     temp_list.sublist(0, 9, subList);
-    std::cout << temp_list.to_string()<< '\n';
-    std::cout << subList.to_string() << '\n';
+    std::cout << temp_list.to_string(lambda)<< '\n';
+    std::cout << subList.to_string(lambda) << '\n';
 }
